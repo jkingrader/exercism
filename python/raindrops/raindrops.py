fa18b724b9099a08cd37c917195485b266bf32ba
Janet King <janet.kingrader@gmail.com>
2020-08-08 21:01:43 -0700

    Add RainDrops

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,17 @@
+def convert(number: int = 0) -> str:
+    pass
+    # if number == 0:
+    #     raindrops_str = ""
+    # else:
+    #     raindrops = "Pling"
+
+    # if number % 5 == 0
+    #     raindrops_str = raindrops_str + "Plang"
+
+    # if number % 7 == 0
+    #     raindrops_str = raindrops_str + "Plong"
+
+    # if raindrops_str == ""
+    #     raindrops_str = f{"number"}
+
+    # return raindrops_str
