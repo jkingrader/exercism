4e7f3a9271730b93f93be4ff421e2e4d88982af9
Janet King <janet.kingrader@gmail.com>
2020-08-10 22:17:43 -0700

    Solve matrix

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,26 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        matrix_rows = matrix_string.split("\n")
+        num_rows = len(matrix_rows)
+        
+        self.matrix = []
+        row_idx = 0
+        while row_idx < num_rows:
+            row_strs = matrix_rows[row_idx].split(" ")
+            row_vals = [int(n) for n in row_strs]
+            self.matrix.append(row_vals)
+            row_idx += 1
+        self.num_rows = num_rows
+        self.num_cols = len(self.matrix[0])
 
     def row(self, index):
-        pass
+        return self.matrix[index-1]
 
     def column(self, index):
-        pass
+        col = []
+ 
+        for row in range(0, self.num_rows):
+            col.append(self.matrix[row][index-1])
+      
+        return col
+
