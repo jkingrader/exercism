71c3ddc8392e905bd5f8d2df5bc50f12444dae85
Janet King <janet.kingrader@gmail.com>
2020-08-11 20:47:30 -0700

    Re-factored matrix init function

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,17 +1,9 @@
 class Matrix:
     def __init__(self, matrix_string):
-        matrix_rows = matrix_string.split("\n")
-        num_rows = len(matrix_rows)
-        
+ 
         self.matrix = []
-        row_idx = 0
-        while row_idx < num_rows:
-            row_strs = matrix_rows[row_idx].split(" ")
-            row_vals = [int(n) for n in row_strs]
-            self.matrix.append(row_vals)
-            row_idx += 1
-        self.num_rows = num_rows
-        self.num_cols = len(self.matrix[0])
+        for row in matrix_string.split("\n"):
+            self.matrix.append([int(n) for n in row.split(" ")])
 
     def row(self, index):
         return self.matrix[index-1]
