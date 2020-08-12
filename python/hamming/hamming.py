5a2a94e9c42385b13d9f5d8e2fda358b4ce55219
Janet King <janet.kingrader@gmail.com>
2020-08-12 01:10:36 -0700

    Solve hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,13 @@
 def distance(strand_a, strand_b):
-    pass
+
+    strand_distance = 0
+
+    if len(strand_a) != len(strand_b):
+        raise ValueError(f"Length of strand_a ({len(strand_a)}) must equal length of strand_b ({len(strand_b)})")
+    else:
+        if len(strand_a) > 0:
+            # for x in list(zip(strand_a, strand_b)):
+            #     if (x[0] != x[1]):
+            #         strand_distance += 1       
+            strand_distance = sum([1 if x[0] != x[1] else 0 for x in list(zip(strand_a, strand_b))])
+        return strand_distance
