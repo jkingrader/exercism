3a515bf1f97d833e97fda81137beefbe54dce925
Janet King <janet.kingrader@gmail.com>
2020-08-12 22:32:26 -0700

    Solve isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,8 @@
 def is_isogram(string):
-    pass
+    lower_string = string.lower()
+    for idx in range(len(lower_string)):
+        if lower_string[idx] != " " and lower_string[idx] != "-":
+            if lower_string.count(lower_string[idx]) > 1:
+                return False
+
+    return True
