317950ffb654966a175631ad2f1518e6b1aee432
Janet King <janet.kingrader@gmail.com>
2020-08-16 23:15:16 -0700

    Solve word_count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,25 @@
 def count_words(sentence):
-    pass
+    import re
+
+    # substitute a blank space for non-alphanumeric characters or non-apostrophe
+    lower_sentence = re.sub("[^0-9a-zA-Z']+"," ", sentence.lower())
+    
+    # replace apostrophes at beginning or end of words
+    lower_sentence = lower_sentence.replace("' ", " ").replace(" '", " ")
+    
+    # brute force - remove all apostrophes at beginning or end of string
+    while len(lower_sentence) > 0 and lower_sentence[0] == "'":
+        lower_sentence = lower_sentence[1:]
+    while len(lower_sentence) > 0 and lower_sentence[-1] == "'":
+        lower_sentence = lower_sentence[:-1]
+
+    # remove all apostrophes at beginning or end of string
+    # lower_sentence.lstrip("'").rstrip("'")
+
+      # initialize dictionary mapping words to counts
+    result = {}
+    words = lower_sentence.split()
+    for word in set(words):
+        result.update( {word : words.count(word)} )
+    
+    return result
